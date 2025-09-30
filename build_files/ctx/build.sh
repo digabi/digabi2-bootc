ecca77bbb629f6c106870f573d3ba1d58043b309
Henna Haahti <84767854+aateekoohenna@users.noreply.github.com>
2025-09-30 15:41:10 +0300

    change os branding

--- build_files/ctx/build.sh
+++ build_files/ctx/build.sh
@@ -2,12 +2,16 @@
 
 set -ouex pipefail
 
-dnf5 install -y gdm gnome-kiosk-script-session chromium
+dnf5 install -y gdm gnome-kiosk-script-session chromium fedora-logos
 
 useradd -m digabi
 passwd -d digabi
 
 cp -r /ctx/filesystem/* /
+cp /ctx/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
 
 sed -i "s/{{BUILD_REVISION}}/$BUILD_REVISION/" /usr/share/digabi2-bootc/index.html
 sed -i "s/{{BUILD_TIMESTAMP}}/$BUILD_TIMESTAMP/" /usr/share/digabi2-bootc/index.html
+sed -i "s/{{BUILD_REVISION}}/$BUILD_REVISION/" /usr/lib/os-release
+
+echo "digabi2-bootc $BUILD_REVISION" > /usr/lib/fedora-release
